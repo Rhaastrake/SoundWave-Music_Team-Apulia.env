0520eed04619214e3ac2e303a631c94ae073a98e
Davide <davide.barbieri1@outlook.it>
2026-07-28 20:44:35 +0200

    feat(KAN-23): add concerts page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,39 @@
 import { Routes } from '@angular/router';
-import { AlbumDetailComponent } from './components/album-detail/album-detail';
-import { ArtistDetailComponent } from './components/artist-detail/artist-detail';
-import { CatalogPageComponent } from './components/catalog-page/catalog-page';
-import { FavoritesPageComponent } from './components/favorites-page/favorites-page';
 import { HomePlaceholderComponent } from './components/home-placeholder/home-placeholder';
-import { BookingComponent } from './components/booking-page/booking-page';
-import { NotFoundComponent } from './components/not-found/not-found';
-import { TicketsComponent } from './components/tickets/tickets';
-import { ConcertPageComponent } from './components/concert-page/concert-page';
 
 export const routes: Routes = [
   { path: '', component: HomePlaceholderComponent },
-  { path: 'catalog', component: CatalogPageComponent },
-  { path: 'music/:id', component: AlbumDetailComponent },
-  { path: 'artist/:id', component: ArtistDetailComponent },
-  { path: 'tickets/:concertId', component: TicketsComponent },
-  { path: 'favorites', component: FavoritesPageComponent },
-  { path: 'concerts', component: ConcertPageComponent},
-  { path: 'prenotazioni', component: BookingComponent },
-  { path: 'not-found', component: NotFoundComponent },
+  {
+    path: 'catalog',
+    loadComponent: () => import('./components/catalog-page/catalog-page').then((m) => m.CatalogPageComponent),
+  },
+  {
+    path: 'music/:id',
+    loadComponent: () => import('./components/album-detail/album-detail').then((m) => m.AlbumDetailComponent),
+  },
+  {
+    path: 'artist/:id',
+    loadComponent: () => import('./components/artist-detail/artist-detail').then((m) => m.ArtistDetailComponent),
+  },
+  {
+    path: 'tickets/:concertId',
+    loadComponent: () => import('./components/tickets/tickets').then((m) => m.TicketsComponent),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./components/favorites-page/favorites-page').then((m) => m.FavoritesPageComponent),
+  },
+  {
+    path: 'concerts',
+    loadComponent: () => import('./components/concert-page/concert-page').then((m) => m.ConcertPageComponent),
+  },
+  {
+    path: 'prenotazioni',
+    loadComponent: () => import('./components/booking-page/booking-page').then((m) => m.BookingComponent),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () => import('./components/not-found/not-found').then((m) => m.NotFoundComponent),
+  },
   { path: '**', redirectTo: '/not-found' },
 ];
