b15bd3d6c93a0cc46b26aee8685418b537af9315
Rhaastrake <garofalomichele2001@gmail.com>
2026-07-29 09:49:20 +0200

    Fixed burger button menu not opening

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -14,14 +13,19 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
     MatListModule,
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class HeaderComponent {
-  isMenuOpen = signal(false);
+  readonly isMenuOpen = signal(false);
+
+  readonly menuIcon = computed(() => (this.isMenuOpen() ? 'close' : 'menu'));
+
+  readonly menuLabel = computed(() => (this.isMenuOpen() ? 'Chiudi menu' : 'Apri menu'));
+
+  readonly exactMatch = { exact: true };
 
   toggleMenu(): void {
     this.isMenuOpen.update((prev) => !prev);
