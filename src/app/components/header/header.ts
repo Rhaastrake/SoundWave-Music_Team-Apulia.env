551d9af9f4ea2239b457bffa70250ec3938bc218
Rhaastrake <garofalomichele2001@gmail.com>
2026-07-29 10:08:34 +0200

    feat(SW-14): Added theme switch button into the navbar

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,11 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -14,16 +16,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatTooltipModule,
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class HeaderComponent {
+  protected readonly themeService = inject(ThemeService);
+
   readonly isMenuOpen = signal(false);
 
   readonly menuIcon = computed(() => (this.isMenuOpen() ? 'close' : 'menu'));
 
   readonly menuLabel = computed(() => (this.isMenuOpen() ? 'Chiudi menu' : 'Apri menu'));
+
+  readonly themeIcon = computed(() => (this.themeService.isDark() ? 'brightness_4' : 'brightness_6'));
+
+  readonly themeLabel = computed(() =>
+    this.themeService.isDark() ? 'Passa al tema chiaro' : 'Passa al tema scuro',
+  );
 
   readonly exactMatch = { exact: true };
 
@@ -33,5 +44,9 @@ export class HeaderComponent {
 
   closeMenu(): void {
     this.isMenuOpen.set(false);
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggle();
   }
 }
